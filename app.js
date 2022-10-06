7a9d49db0097c88dd440476a4f8e98308fdaea5c
BONSAM3 <105445014+BONSAM3@users.noreply.github.com>
2022-10-06 15:14:51 +0100

    fnfg

---
+++ app.js
@@ -0,0 +1,88 @@
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+    toggleItem(item);
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+  reset: true,
+});
+sr.reveal(
+  ".home__title, .popular__container, .subscribe__container, .footer__container"
+);
+sr.reveal(".home__description,.footer__info", { delay: 500 });
+sr.reveal(".home__search", { delay: 600 });
+sr.reveal(".home__images", { delay: 800, origin: "bottom" });
+sr.reveal(".home__img", { interval: 100 });
+sr.reveal(".value__images .contact__content", { origin: "left" });
+sr.reveal(".value__content", { origin: "right" });
+sr.reveal(".value__content .contact__images", { origin: "right" });
